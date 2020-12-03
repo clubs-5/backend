0255e1fee2506f1667a2990af5145fc9af72cf7b
veritaspoutine <14975224+veritaspoutine@users.noreply.github.com>
2020-12-03 14:53:41 +0800

    fixed pyarrow deserialize methods

--- backend/read_df_from_redis.py
+++ backend/read_df_from_redis.py
@@ -1,14 +1,15 @@
 import pyarrow as pa
 import redis
+import pandas as pd
 
 # 宣告 redis 連線
 r = redis.Redis(host='master.tibame', port=6379, db=0)
+context = pa.default_serialization_context()
 
 # 從 redis 讀取 recomm 資料
 data = r.get('recomm')
 
 # 反序列化
-
-df = pa.deserialize(data)
+df = pd.DataFrame.from_dict(context.deserialize(data))
 
 df.head()
